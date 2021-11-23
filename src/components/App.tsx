891100f47bd69ec991204c7553a31f548f266c41
hanul-cha <ckgks3014@naver.com>
2021-11-23 19:54:10 +0900

    route v6에서 바뀐걸 몰랐네....

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { BrowserRouter,
+  Routes,
+  Route,
+  Link } from "react-router-dom"
+import Home from '../routes/Home';
+import Detail from '../routes/Detail';
 
-function App() {
+function App() :JSX.Element{
   return (
-    <div className="App"></div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:id" element={<Detail />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/routes/Detail.tsx
@@ -0,0 +1,7 @@
+const Detail = () => {
+    return (
+        <div><h2>Detail</h2></div>
+    )
+}
+
+export default Detail
