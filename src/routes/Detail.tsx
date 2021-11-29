aa4c4fe1b6c33319684189d356db2161934ea88e
hanul-cha <ckgks3014@naver.com>
2021-11-29 13:27:59 +0900

    옵셔널 체이닝

--- src/routes/Detail.tsx
+++ src/routes/Detail.tsx
@@ -6,6 +6,10 @@ import styled from 'styled-components';
 
 const Detail = () => {
 
+    type PostType = {
+        myBg :any
+    }
+
     //날려줄 쿼리 넘겨줄 인자는 !를 넣어서 꼭필요한 인자로 지정해주자!
     const GET_MOVIE = gql`
         query getMovie($id :Int!){
@@ -19,6 +23,43 @@ const Detail = () => {
         }
     `;
 
+    const Container = styled.div`
+      height: 100vh;
+      background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+      width: 100%;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      color: white;
+    `;
+
+    const Column = styled.div`
+      margin-left: 10px;
+      width: 50%;
+    `;
+
+    const Title = styled.h1`
+      font-size: 65px;
+      margin-bottom: 15px;
+    `;
+
+    const Subtitle = styled.h4`
+      font-size: 35px;
+      margin-bottom: 10px;
+    `;
+
+    const Description = styled.p`
+      font-size: 28px;
+    `;
+    
+    const Poster = styled.div<PostType>`
+      width: 25%;
+      height: 60%;
+      background-color: transparent;
+      background-image: url(${props => props.myBg});
+      background-size: cover;
+      background-position: center center;
+    `;
 
 
     const { id }  = useParams()
@@ -27,17 +68,19 @@ const Detail = () => {
             id: Number(id)
         }
     })
-
-    
-
     console.log(loading, data, id)
 
-
     return (
-        <div>
-        {loading && "Loading"}
-        {!loading && data.movie && data.movie.title}
-        </div>
+        <Container>
+           <Column>
+            <Title>{loading ? "Loading..." : data.movie.title}</Title>
+                <Subtitle>
+                    {data?.movie?.language} ㆍ {data?.movie?.rating}
+                </Subtitle>
+                <Description>{data?.movie?.description_intro}</Description>
+           </Column> 
+           <Poster myBg={data?.movie?.medium_cover_image}></Poster>
+        </Container>
     )
 }
 
